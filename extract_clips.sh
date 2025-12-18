ebf1f349020a0029af79a9dfbf37bfb91f8acb43
brianchc <brianchc@login-03.cm.cluster>
2025-12-18 00:24:49 -0800

    split data processing scripts

--- extract_clips.sh
+++ extract_clips.sh
@@ -4,9 +4,3 @@ python extract_clips_parallel.py \
     --output_folder /scratch/m000051-pm04/brianchc/handctrl_data_v1 \
     --egovid5M_folder /scratch/m000051-pm04/brianchc/egovid_5M/ \
     --extraction_method decord # for faster extraction, use decord
-
-exit
-
-# Extract poses from videos
-python label_clips_parallel.py \
-    --input_folder /scratch/m000051-pm04/brianchc/handctrl_data_v1
---
+++ label_clips.sh
@@ -0,0 +1,4 @@
+# Extract poses from videos
+python label_clips_parallel.py \
+    --input_folder /scratch/m000051-pm04/brianchc/handctrl_data_v1
+
