75cce38434ec6d656083ceb1fc08e5a9690ef777
brianchc <brianchc@login-01.cm.cluster>
2025-12-18 22:46:41 -0800

    update scripts

--- extract_clips.sh
+++ extract_clips.sh
@@ -1,10 +1,11 @@
 # Extract clips from videos
 mode=$1 # debug (lab servers) or full (marlowe)
+out_folder=$2
 
 if [ "$mode" == "full" ]; then
 python extract_clips_parallel.py \
     --input_folder /scratch/m000051-pm04/brianchc/ego4d_fho/v2/full_scale \
-    --output_folder /scratch/m000051-pm04/brianchc/handctrl_data_v1 \
+    --output_folder /scratch/m000051-pm04/brianchc/$out_folder \
     --egovid5M_folder /scratch/m000051-pm04/brianchc/egovid_5M/ \
     --extraction_method decord \
     --num_workers 16
