8fa1e85859e735827cae8284645a9376dab5a09d
brianchc <brianchc@login-01.cm.cluster>
2025-12-18 22:38:39 -0800

    update scripts params

--- extract_clips.sh
+++ extract_clips.sh
@@ -6,7 +6,8 @@ python extract_clips_parallel.py \
     --input_folder /scratch/m000051-pm04/brianchc/ego4d_fho/v2/full_scale \
     --output_folder /scratch/m000051-pm04/brianchc/handctrl_data_v1 \
     --egovid5M_folder /scratch/m000051-pm04/brianchc/egovid_5M/ \
-    --extraction_method decord
+    --extraction_method decord \
+    --num_workers 16
 else
 python extract_clips_parallel.py \
     --input_folder /miele/data/ego4d_fho/v2/full_scale/ \
--- label_clips.sh
+++ label_clips.sh
@@ -3,7 +3,8 @@ mode=$1 # debug (lab servers) or full (marlowe)
 
 if [ "$mode" == "full" ]; then
 python label_clips_parallel.py \
-    --input_folder /scratch/m000051-pm04/brianchc/handctrl_data_v1
+    --input_folder /scratch/m000051-pm04/brianchc/handctrl_data_v1 \
+    --num_workers 16
 else
 python label_clips_parallel.py \
     --input_folder ./test_dataset/handctrl_data_v1
